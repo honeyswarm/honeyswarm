d91fb908f73daf83956a612a0876d00428270b9e
KevTheHermit <kevthehermit@gmail.com>
2020-02-11 00:07:26 +0000

    Force salt minion to python3

--- honeyswarm/templates/hive_registration.sh
+++ honeyswarm/templates/hive_registration.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
+
+# This forces Python3 instal
+# For supported versions - https://github.com/saltstack/salt-bootstrap#python-3-support
+
 curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
-sudo sh bootstrap-salt.sh -A {{honeyswarm_host}} -i {{salt_minion_id}}
+sudo sh bootstrap-salt.sh -x python3 -A {{honeyswarm_host}} -i {{salt_minion_id}}
