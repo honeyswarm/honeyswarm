0170d9259dcb48b1fb7defb5cca65439090dda85
KevTheHermit <kevthehermit@gmail.com>
2020-02-17 15:43:14 +0000

    Doc Strings in saltapi

--- honeyswarm/saltapi.py
+++ honeyswarm/saltapi.py
@@ -1,8 +1,12 @@
 import os
 from pepper.libpepper import Pepper
+from pepper.exceptions import PepperException
+
+# https://docs.saltstack.com/en/latest/ref/clients/#python-client-api
 
 class PepperApi():
     def __init__(self):
+        self.authenticated = False
         self.api = Pepper(os.environ.get("SALT_HOST"))
         self.api_auth()
 
@@ -11,6 +15,7 @@ class PepperApi():
     def api_auth(self):
         print("Pepper Auth")
         self.api.login(os.environ.get("SALT_USERNAME"), os.environ.get("SALT_SHARED_SECRET"), 'sharedsecret')
+        self.authenticated = True
 
 
     def salt_ping(self, targets):
@@ -18,18 +23,34 @@ class PepperApi():
             api_response = self.api.low([{'client': 'local', 'tgt': targets, 'fun': 'test.ping'}])
             responses = api_response['return']
             return responses
+        except PepperException as p:
+            if p == 'Authentication denied':
+                self.api_auth()
         except Exception as err:
             print(err)
             return []
 
     def salt_keys(self):
-        """Gets a list of keys from the salt server"""
+        """
+        Queries the Salt Master for all Minion keys
+
+        Returns:
+            dict: A dictionary with 3 lists for minions, pre-minions and rejected minions
+        """
         api_reponse = self.api.low([{'client': 'wheel', 'fun': 'key.list_all'}])
         if api_reponse['return'][0]['data']['success']:
             return api_reponse['return'][0]['data']['return']
 
     def accept_key(self, minion_id):
-        """Given a Minion ID will accept the key in the salt stack"""
+        """
+        Tells the Salt Master to Accept a Key by its name
+
+        Args:
+            minion_id (str): Minion ID - should be a Mongo ID from `Hive` modal 
+
+        Returns:
+            dict: A dictionary with the ID for any accepted minions. 
+        """
         api_reponse = self.api.low([{'client': 'wheel', 'fun': 'key.accept', 'match':minion_id}])
         if api_reponse['return'][0]['data']['success']:
             return api_reponse['return'][0]['data']['return']
@@ -56,21 +77,47 @@ class PepperApi():
         api_reponse = self.api.low([{'client': 'local', 'tgt': target, 'fun': function}])
         return api_reponse['return'][0]
 
-    def apply_state(self, target, state_name):
+    def apply_state(self, target, args_list):
         """
-        Basic Function Runner; for things like test.ping it is up to the receiving function to parse the data it needs.
-        Makes Async Call returns Job ID
+        Tells the Salt Master to apply a given state to a named minion
+
+        Args:
+            target (str): Minion ID - should be a Mongo ID from `Hive` modal 
+            args_list (list): List of args. First element in list MUST be the Salt State to apply
+
+        Returns:
+            str: Salt Job ID 
         """
-        api_reponse = self.api.low([{'client': 'local_async', 'tgt': target, 'fun': "state.apply", 'arg': [state_name]}])
+        api_reponse = self.api.low([{'client': 'local_async', 'tgt': target, 'fun': "state.apply", 'arg': args_list}])
         print(api_reponse)
         return api_reponse['return'][0]['jid']
 
     def lookup_job(self, job_id):
-        api_response = self.api.lookup_jid(job_id)
-        if len(api_response['return'][0]) == 0:
-            return None
-        else:
-            return api_response['return'][0]
+        """
+        Query the Salt Master for a job by its Salt Job ID
+
+        Args:
+            job_id (str): Salt Job Id should be taken from PepperJobs Model 
+
+        Returns:
+            dict/None: Return None if job still pending else dictionary of all state modules and their outputs.  
+        """
+        # This is lazy but as this job runs frequently use it to check auth state
+        try:
+            api_response = self.api.lookup_jid(job_id)
+            if len(api_response['return'][0]) == 0:
+                return None
+            else:
+                return api_response['return'][0]
+        except PepperException as p:
+            if p == 'Authentication denied':
+                self.api_auth()
+        except Exception as err:
+            print(err)
+        return None
+
+        
+
         
 
 pepper_api = PepperApi()
