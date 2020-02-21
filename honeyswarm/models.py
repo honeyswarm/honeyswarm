bb81cb24d0a8e85212e37f831bf4d77f4bb322df
KevTheHermit <kevthehermit@gmail.com>
2020-02-21 04:42:07 +0000

    Fix model order

--- honeyswarm/models.py
+++ honeyswarm/models.py
@@ -3,10 +3,9 @@ from datetime import datetime
 from flask_login import UserMixin
 
 
-class User(UserMixin, Document):
-    email = StringField(unique=True)
-    password = StringField()
+class Frame(Document):
     name = StringField()
+    description = StringField()
 
 
 class Honeypot(Document):
@@ -14,7 +13,6 @@ class Honeypot(Document):
     honey_type = StringField()
     description = StringField()
     pillar = ListField()
-
 
 class Hive(Document):
     name = StringField(unique=True)
@@ -24,7 +22,7 @@ class Hive(Document):
     last_seen = DateTimeField()
     grains = DictField(default={'osfullname': 'Not Polled', 'ipv4':[]})
     honeypots = ListField(ReferenceField(Honeypot), default=[])
-
+    frame = ReferenceField(Frame)
 
 class PepperJobs(Document):
     job_id = StringField()
@@ -36,4 +34,9 @@ class PepperJobs(Document):
     completed_at = DateTimeField()
     job_response = StringField()
     hive = ReferenceField(Hive)
-    
+
+
+class User(UserMixin, Document):
+    email = StringField(unique=True)
+    password = StringField()
+    name = StringField()
