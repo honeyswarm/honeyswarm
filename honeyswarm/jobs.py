c65aec24acd091e90c91cfc83296fdf679db3d85
KevTheHermit <kevthehermit@gmail.com>
2020-02-23 04:17:21 +0000

    Start Jobs Poller

--- honeyswarm/jobs.py
+++ honeyswarm/jobs.py
@@ -1,4 +1,5 @@
 import os
+import json
 from uuid import uuid4
 from datetime import datetime
 
@@ -25,3 +26,28 @@ def jobs_list():
         "jobs.html",
         job_list=job_list
         )
+
+
+@jobs.route('/jobs/poll')
+@login_required
+def jobs_poll():
+
+    json_response = {"success": False, "job_complete": False}
+
+    try:
+        job_id = request.forms.get('job_id')
+        job = PepperJobs.objects(id=job_id)
+
+        api_check = pepper_api.lookup_job(job.job_id)
+        hive_id = str(job['hive']['id'])
+        if api_check:
+            job.complete = True
+            job.job_response = json.dumps(api_check['data'][hive_id], sort_keys=True, indent=4)
+            job.completed_at = datetime.utcnow
+        job.save()
+        json_response['success'] = True
+        json_response['job_complete'] = job.complete
+    except Exception as err:
+        json_response['message'] = err
+
+    return jsonify(json_response)
